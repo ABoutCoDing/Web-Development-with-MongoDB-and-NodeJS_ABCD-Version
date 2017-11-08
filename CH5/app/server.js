77c97d3d65eccf0eb3414d8583aabe50fd9da760
jamie han <jamie@snowkiwi.com>
2017-11-09 01:02:57 +0900

    commit

--- CH5/app/server.js
+++ CH5/app/server.js
@@ -5,8 +5,6 @@ app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
 app = config(app);
 
-
-
 app.listen(app.get('port'), function() {
     console.log('Server up: http://localhost:' + app.get('port'));
 });
--- CH5/app/server/configure.js
+++ CH5/app/server/configure.js
@@ -6,32 +6,33 @@ var path = require('path'),
     cookieParser = require('cookie-parser'),
     morgan = require('morgan'),
     methodOverride = require('method-override'),
-    errorHandler = require('errorhandler');
+    errorHandler = require('errorhandler'),
+    moment = require('moment');
 
 module.exports = function(app) {
     app.use(morgan('dev'));
     // app.use(bodyParser({
     //     uploadDir:path.join(__dirname, 'public/upload/temp')
     // }));
-    // app.use(bodyParser.urlencoded({'extended': true}));
-    // app.use(bodyParser.json());
-    // app.use(methodOverride());
-    // app.use(cookieParser('some-secret-value-here'));
-    
-    app.use(function(req, res, next) {
-        console.log('custom middleware');
-        return next();
-    });
+    app.use(bodyParser.urlencoded({'extended': true}));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+    app.use(cookieParser('some-secret-value-here'));
     routes(app);
-    app.use('/public', express.static(path.join(__dirname, '../public')));
+    // app.use('/public', express.static(path.join(__dirname, '../public')));
     if('development' === app.get('env')) {
         app.use(errorHandler());
     }
-    
+
     app.engine('handlebars', exphbs.create({
         defaultLayout: 'main',
         layoutsDir: app.get('views') + '/layouts',
-        partialsDir: [app.get('views') + '/partials']
+        partialsDir: [app.get('views') + '/partials'],
+        helpers: {
+            timeago: function(timestamp) {
+                return moment(timestamp).locale('ko').startOf('minute').fromNow();
+            }
+        }
     }).engine);
     app.set('view engine', 'handlebars');
 
