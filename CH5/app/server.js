a4ace929f8a207cfc6ee27186d19cfc42a9f57ad
jamie han <jamie@snowkiwi.com>
2017-11-15 17:22:43 +0900

    commit

--- CH5/app/server.js
+++ CH5/app/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
-config = require('./server/configure');
-app = express();
+    config = require('./server/configure');
+    app = express();
+    
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
 app = config(app);
